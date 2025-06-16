4a37dc6d5ee854ea9f6658e2d73a5622b4068127
JuHyeonuk <susuk0243@naver.com>
2025-06-16 11:08:25 +0000

    최종 수정 2차

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ plt.rcParams['axes.unicode_minus'] = False
 인구10만명당 = [0.4, 2.2, 6.2, 7.8, 6.9, 9.2, 8.7, 10.9, 15.4]
 
 # 그래프 그리기
-fig, ax1 = plt.subplots(figsize=(8, 5))
+fig_age, ax1 = plt.subplots(figsize=(8, 5))
 bar = ax1.bar(연령대, 환자수, color='gray', label='온열질환자 수')
 ax1.set_ylabel('온열질환자 수 (명)', fontproperties=font_prop)
 ax1.set_xlabel('연령대', fontproperties=font_prop)
@@ -187,7 +187,7 @@ elif menu == "온열질환 예방 대응 방안":
         st.image(map_img, caption="2024년 전국 고령 인구 비율", use_container_width=True)
     with col2:
         st.markdown("#### 📊 연령대별 온열질환자 수")
-        st.image(bar_img, caption="연령별 온열질환자 수 및 인구 10만명당 환자수", use_container_width=True)
+        st.pyplot(fig_age)
 
     st.markdown("#### 📈 고령 인구 비율 vs 온열환자 수")
     st.image(img_buffer, caption="고령 인구 비율과 온열환자 수의 상관 관계", use_container_width=True)
@@ -265,7 +265,7 @@ elif menu == "전체 보기":
         st.image(map_img, caption="2024년 전국 고령 인구 비율", use_container_width=True)
     with col2:
         st.markdown("#### 📊 연령대별 온열질환자 수")
-        st.pyplot(fig)
+        st.pyplot(fig_age)
 
     st.markdown("#### 📈 고령 인구 비율 vs 온열환자 수")
     st.image(img_buffer, caption="고령 인구 비율과 온열환자 수의 상관 관계", use_container_width=True)
